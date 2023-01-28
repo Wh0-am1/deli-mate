9e5ec8d530b397e0f01d57564675a54e74dcd567
Wh0-am1 <sidheeque4441234@gmail.com>
2023-01-28 16:39:10 +0400

    add comments

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./login.css";
-
+//comment
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
